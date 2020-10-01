af4d8093c44b34dbacbb675caa95f76376fe9f14
Naveen Sundar <naveennvrgup@gmail.com>
2020-10-01 14:36:02 +0530

    2

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,5 +1,6 @@
 public class HelloJenkins{
     public static void main(String args[]){
+        System.out.println("hello world !!!");
         for(int i=0;i<10;i++){
             System.out.println("Jenkins from git .... "+i);
         }
